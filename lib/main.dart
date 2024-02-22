a697d02b413176a82fa705cbaea0d6d7349081e6
BlackCheppun <aghouilesilyes1@gmail.com>
2024-02-22 16:35:49 +0100

    added page navigation ( not tested yet )

--- lib/main.dart
+++ lib/main.dart
@@ -31,56 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: () {}, child: Text('hello there')),
-            FloatingActionButton(
-              onPressed: () {},
-              child: const Icon(Icons.add),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: navbar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class navbar extends StatefulWidget {
-  const navbar({
-    super.key,
-  });
-
-  @override
-  State<navbar> createState() => _navbarState();
-}
-
-class _navbarState extends State<navbar> {
   int _selectedindex = 0;
-
-  void _onPress(int index) {
+  void _onDestinationTapped(int index) {
     setState(() {
       _selectedindex = index;
     });
@@ -88,18 +40,79 @@ class _navbarState extends State<navbar> {
 
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (_selectedindex) {
+      case 0:
+        page = randompage();
+        break;
+      case 1:
+        page = randompage();
+        break;
+      case 2:
+        page = randompage();
+        break;
+      case 3:
+        page = randompage();
+        break;
+      default:
+        page = randompage();
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: page,
+      ),
+      bottomNavigationBar: navbar(
+        currentIndex: _selectedindex,
+        onTap: _onDestinationTapped,
+      ),
+    );
+  }
+}
+
+class randompage extends StatefulWidget {
+  const randompage({super.key});
+
+  @override
+  State<randompage> createState() => _randompageState();
+}
+
+class _randompageState extends State<randompage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class navbar extends StatefulWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+  const navbar({super.key, required this.currentIndex, required this.onTap});
+
+  @override
+  State<navbar> createState() => _navbarState();
+}
+
+class _navbarState extends State<navbar> {
+  @override
+  Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return BottomNavigationBar(
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
         BottomNavigationBarItem(
-            icon: Icon(Icons.watch_later_rounded), label: 'Messages'),
-        BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Messages'),
-        BottomNavigationBarItem(icon: Icon(Icons.person_pin), label: 'Messages')
+            icon: Icon(Icons.watch_later_rounded), label: 'Recents'),
+        BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Appels'),
+        BottomNavigationBarItem(icon: Icon(Icons.person_pin), label: 'Profil')
       ],
       selectedItemColor: Theme.of(context).colorScheme.primary,
-      currentIndex: _selectedindex,
-      onTap: _onPress,
+      currentIndex: widget.currentIndex,
+      onTap: widget.onTap,
       unselectedIconTheme: theme.iconTheme,
     );
   }
