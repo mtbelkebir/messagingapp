e47f7509db873644f202d6b0d9bfa797d639d632
BlackCheppun <aghouilesilyes1@gmail.com>
2024-02-24 00:37:29 +0100

    moved the navbar to the widget folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'widgets/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -86,34 +87,5 @@ class _RandompageState extends State<Randompage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
-  }
-}
-
-class Navbar extends StatefulWidget {
-  final int currentIndex;
-  final void Function(int) onTap;
-  const Navbar({super.key, required this.currentIndex, required this.onTap});
-
-  @override
-  State<Navbar> createState() => _NavbarState();
-}
-
-class _NavbarState extends State<Navbar> {
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.watch_later_rounded), label: 'Recents'),
-        BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Appels'),
-        BottomNavigationBarItem(icon: Icon(Icons.person_pin), label: 'Profil')
-      ],
-      selectedItemColor: Theme.of(context).colorScheme.primary,
-      currentIndex: widget.currentIndex,
-      onTap: widget.onTap,
-      unselectedIconTheme: theme.iconTheme,
-    );
   }
 }
