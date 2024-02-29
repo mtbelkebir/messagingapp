2f60efe36b5f2b1563f2c750623bfd105d3aa063
BlackCheppun <aghouilesilyes1@gmail.com>
2024-02-29 17:21:01 +0100

    added profile pic to the card

--- lib/widgets/pinnedCard.dart
+++ lib/widgets/pinnedCard.dart
@@ -12,20 +12,28 @@ class PpinnedCardState extends State<PinnedCard> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Center(
-      child: Card(
-        clipBehavior: Clip.hardEdge,
-        child: InkWell(
-          
-            splashColor: theme.primaryColorLight,
-            onTap: () {
-              debugPrint('Card tapped.');
-            },
-            child: const SizedBox(
+        child: Card(
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+          splashColor: theme.primaryColorLight,
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: const SizedBox(
               height: 100,
-              width: 300,
-              child: Text('A card that can be tapped'),
-            )),
-      ),
-    );
+              width: 200,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('images/person.jpg'),
+                    ),
+                    Text('A card that can be tapped'),
+                  ],
+                ),
+              ))),
+    ));
   }
 }
